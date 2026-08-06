121367137f4b7724fc236b36593765575cc7a60e
johnlatif19 <johnlatif17@gmail.com>
2026-08-06 21:36:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
